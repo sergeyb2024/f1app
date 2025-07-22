f2b1c936eea7e9065bc736eb6eadd8c24c1d3cec
Sea-Moryak <164082172+sergeyb2024@users.noreply.github.com>
2025-07-22 11:40:54 -0400

    Create raceSchedule.ts

---
+++ data/raceSchedule.ts
@@ -0,0 +1,28 @@
+import { Race } from '@/types';
+
+export const raceSchedule: Race[] = [
+   name: "Louis Vuitton Australian GP", circuit: "Melbourne Grand Prix Circuit", location: "Melbourne", raceDate: new Date("2025-03-16T12:00:00") },
+      { name: "Heineken Chinese GP", circuit: "Shanghai International Circuit", location: "Shanghai", raceDate: new Date("2025-03-23T12:00:00") },
+      { name: "Lenovo Japanese GP", circuit: "Suzuka International Racing Course", location: "Suzuka", raceDate: new Date("2025-04-06T12:00:00") },
+      { name: "Gulf Air Bahrain GP", circuit: "Bahrain International Circuit", location: "Sakhir", raceDate: new Date("2025-04-13T12:00:00") },
+      { name: "STC Saudi Arabian GP", circuit: "Jeddah Street Circuit", location: "Jeddah", raceDate: new Date("2025-04-20T12:00:00") },
+      { name: "Crypto.com Miami GP", circuit: "Miami International Autodrome", location: "Miami", raceDate: new Date("2025-05-04T12:00:00") },
+      { name: "AWS Made in Italy Emilia Romagna GP", circuit: "Autodromo Enzo e Dino Ferrari", location: "Imola", raceDate: new Date("2025-05-18T12:00:00") },
+      { name: "Tag Heuer Monaco GP", circuit: "Circuit de Monaco", location: "Monaco", raceDate: new Date("2025-05-25T12:00:00") },
+      { name: "Aramco Spanish GP", circuit: "Circuit de Barcelona-Catalunya", location: "Barcelona", raceDate: new Date("2025-06-01T12:00:00") },
+      { name: "Pirelli Canadian GP", circuit: "Circuit Gilles-Villeneuve", location: "Montréal", raceDate: new Date("2025-06-15T12:00:00") },
+      { name: "MSC Cruises Austrian GP", circuit: "Red Bull Ring", location: "Spielberg", raceDate: new Date("2025-06-29T12:00:00") },
+      { name: "Qatar Airways British GP", circuit: "Silverstone Circuit", location: "Silverstone", raceDate: new Date("2025-07-06T12:00:00") },
+      { name: "Moët & Chandon Belgian GP", circuit: "Circuit de Spa-Francorchamps", location: "Spa-Francorchamps", raceDate: new Date("2025-07-27T09:00:00") },
+      { name: "Lenovo Hungarian GP", circuit: "Hungaroring", location: "Budapest", raceDate: new Date("2025-08-03T09:00:00") },
+      { name: "Heineken Dutch GP", circuit: "Circuit Park Zandvoort", location: "Zandvoort", raceDate: new Date("2025-08-31T09:00:00") },
+      { name: "Pirelli Italian GP", circuit: "Autodromo Nazionale Monza", location: "Monza", raceDate: new Date("2025-09-07T09:00:00") },
+      { name: "Qatar Airways Azerbaijan GP", circuit: "Baku City Circuit", location: "Baku", raceDate: new Date("2025-09-21T07:00:00") },
+      { name: "Singapore Airlines Singapore GP", circuit: "Marina Bay Street Circuit", location: "Singapore", raceDate: new Date("2025-10-05T08:00:00") },
+      { name: "MSC Cruises United States GP", circuit: "Circuit of the Americas", location: "Austin", raceDate: new Date("2025-10-19T15:00:00") },
+      { name: "Mexico City GP", circuit: "Autodromo Hermanos Rodriguez", location: "Mexico City", raceDate: new Date("2025-10-26T16:00:00") },
+      { name: "MSC Cruises São Paulo GP", circuit: "Autodromo Jose Carlos Pace", location: "São Paulo", raceDate: new Date("2025-11-09T12:00:00") },
+      { name: "Heineken Las Vegas GP", circuit: "Las Vegas Street Circuit", location: "Las Vegas", raceDate: new Date("2025-11-22T23:00:00") },
+      { name: "Qatar Airways Qatar GP", circuit: "Losail International Circuit", location: "Lusail", raceDate: new Date("2025-11-30T11:00:00") },
+      { name: "Etihad Airways Abu Dhabi GP", circuit: "Yas Marina Circuit", location: "Yas Island", raceDate: new Date("2025-12-07T08:00:00") },
+];
