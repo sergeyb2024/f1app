63f1c14b6bc0c1ad066d108985b5d5ab45e13407
Sea-Moryak <164082172+sergeyb2024@users.noreply.github.com>
2025-07-22 11:35:19 -0400

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,27 @@
+export interface Race {
+  name: string;
+  circuit: string;
+  location: string;
+  raceDate: Date;
+}
+
+export interface Upgrade {
+  Year: number;
+  RaceNo: number;
+  RaceName: string;
+  Constructor: string;
+  RequiredEventDisplay: string;
+  ComponentNo: number;
+  UpdatedComponent: string;
+  PrimaryReason: string;
+  GeometricDifferences: string;
+  Description: string;
+}
+
+export interface SavedDetail {
+  id: number;
+  raceName: string;
+  constructor: string;
+  updatedComponent: string;
+  description: string;
+}
